24d507501f0161a504467e6715210417c68584f0
edudiass <46166251+edudiass@users.noreply.github.com>
2019-04-21 21:24:47 -0300

    Asp.NET MCV

---
+++ financas/Controllers/UsuarioController.cs
@@ -0,0 +1,46 @@
+﻿using financas.DAO;
+using financas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace financas.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private UsuarioDAO usuarioDAO;
+
+        public UsuarioController(UsuarioDAO usuarioDAO)
+        {
+            this.usuarioDAO = usuarioDAO;
+        }
+        // GET: Usuario
+        public ActionResult Form()
+        {
+            return View();
+
+
+        }
+
+        public ActionResult Adiciona(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                usuarioDAO.Adiciona(usuario);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Form", usuario);
+            }
+        }
+        public ActionResult Index()
+        {
+            IList<Usuario> usuarios = usuarioDAO.Lista();
+            return View(usuarios);
+        }
+
+    }
+}
---
+++ financas/Entidades/Tipo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace financas.Entidades
+{
+    public enum Tipo
+    {Entrada, Saida
+
+
+    }
+}
